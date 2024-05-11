49deb78ab0f46de3f3a14598b63edd7c5c37ce4e
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-11 10:02:45 -0400

    t

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -10,7 +10,7 @@ server = app.server
 data = {
     'A': [1, 2, 3, 4, 5],
     'B': ['a', 'b', 'c', 'd', 'e'],
-    'C': [True, False, True, False, True]
+    'C': ["c", "d", "d", "E", "True"]
 }
 
 # Creating the DataFrame
@@ -18,11 +18,25 @@ df = pd.DataFrame(data)
 
 app.layout = html.Div([
     html.H1('Joy\'s Scraper'),
-    html.Table(
-        [html.Tr([html.Th(col) for col in df.columns])] +
-        [html.Tr([html.Td(df.iloc[i][col]) for col in df.columns]) for i in range(len(df))]
-    )
+    dcc.Input(id='search-bar', type='text', placeholder='Enter value to search...'),
+    html.Button('Search', id='search-button', n_clicks=0),
+    html.Div(id='search-output')
 ])
+
+@app.callback(
+    Output('search-output', 'children'),
+    [Input('search-button', 'n_clicks')],
+    [Input('search-bar', 'value')]
+)
+def search_value(n_clicks, search_value):
+    if n_clicks > 0 and search_value:
+        result = df[df.eq(search_value).any(1)]
+        if not result.empty:
+            return result.to_html(index=False)
+        else:
+            return "No matching rows found."
+    else:
+        return ""
 
 if __name__ == '__main__':
     app.run_server(debug=True)
