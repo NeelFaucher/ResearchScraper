081b35b0d473fcb64e8b0f4430624e957d75de82
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-11 10:11:27 -0400

    t

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ app.layout = html.Div([
     html.H1('Joy\'s Scraper'),
     dcc.Input(id='search-bar', type='text', placeholder='Enter value to search...'),
     html.Button('Search', id='search-button', n_clicks=0),
-    html.Div(id='search-value'),
-    html.Div(id='dataframe-output')
+    html.Div(id='search-value')
 ])
 
 @app.callback(
@@ -32,21 +31,6 @@ app.layout = html.Div([
 def search_value(n_clicks, search_value):
     if n_clicks > 0 and search_value:
         return f'You searched for: {search_value}'
-    else:
-        return ""
-
-@app.callback(
-    Output('dataframe-output', 'children'),
-    [Input('search-button', 'n_clicks')],
-    [Input('search-bar', 'value')]
-)
-def display_dataframe(n_clicks, search_value):
-    if n_clicks > 0 and search_value:
-        result = df[df.eq(search_value).any(1)]
-        if not result.empty:
-            return result.to_html(index=False)
-        else:
-            return "No matching rows found."
     else:
         return ""
 
