9063f2a64bf9439267cdeedfb75d055ff6038a70
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-11 10:54:16 -0400

    T

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from dash import Dash, html, dcc, Input, Output, dash_table
+from dash import Dash, html, dcc, Input, Output
+import dash_table
 import pandas as pd
-from scrape import scrape_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -9,10 +9,14 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 data = {
-    'A': [1, 2, 3, 4, 5],
-    'B': ['a', 'b', 'c', 'd', 'e'],
-    'C': [True, False, True, False, True]
+    'URL': urls[:2],
+    'Title': titles,
+    'Authors': authors_list,
+    'Date': dates,
+    'Abstract': abstracts
 }
+
+# Create a DataFrame
 df = pd.DataFrame(data)
 
 app.layout = html.Div([
@@ -32,13 +36,11 @@ app.layout = html.Div([
 )
 def search_value(n_clicks, search_value):
     if n_clicks > 0 and search_value:
-        # Here you would ideally use search_value to filter data or perform any other operations
-        # For now, let's just call scrape_data() and display the DataFrame
-        df = scrape_data()
+        filtered_df = df[df['Title'].str.contains(search_value, case=False, regex=False)]
         return dash_table.DataTable(
             id='data-table',
-            columns=[{"name": i, "id": i} for i in df.columns],
-            data=df.to_dict('records')
+            columns=[{"name": i, "id": i} for i in filtered_df.columns],
+            data=filtered_df.to_dict('records')
         )
     else:
         return ""
