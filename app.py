bebae95117004ee33b44ba223bddd1d4fcf1b3e7
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-11 10:45:44 -0400

    T

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, dcc, Input, Output, DataTable, TableColumn
 import pandas as pd
-from scrape import scrape_data  # Import the scrape_data function
+from scrape import scrape_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -8,25 +8,38 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+data = {
+    'A': [1, 2, 3, 4, 5],
+    'B': ['a', 'b', 'c', 'd', 'e'],
+    'C': [True, False, True, False, True]
+}
+df = pd.DataFrame(data)
+
 app.layout = html.Div([
     html.H1("Joy's Scraper"),
-    html.Button('Scrape Data', id='scrape-button', n_clicks=0),
-    html.Div(id='data-display')
+    html.Div([
+        dcc.Input(id='search-bar', type='text', placeholder='Enter value to search...'),
+        html.Button('Search', id='search-button', n_clicks=0),
+        html.Div(id='search-output')
+    ]),
+    html.Div(id='data-table-container')
 ])
 
 @app.callback(
-    Output('data-display', 'children'),
-    [Input('scrape-button', 'n_clicks')]
+    Output('search-output', 'children'),
+    [Input('search-button', 'n_clicks')],
+    [Input('search-bar', 'value')]
 )
-def display_data(n_clicks):
-    if n_clicks > 0:
-        df = scrape_data()  # Call the scrape_data function
-        # Convert DataFrame to HTML table
-        table = html.Table([
-            html.Thead(html.Tr([html.Th(col) for col in df.columns])),
-            html.Tbody([html.Tr([html.Td(df.iloc[i][col]) for col in df.columns]) for i in range(len(df))])
-        ])
-        return table
+def search_value(n_clicks, search_value):
+    if n_clicks > 0 and search_value:
+        # Here you would ideally use search_value to filter data or perform any other operations
+        # For now, let's just call scrape_data() and display the DataFrame
+        df = scrape_data()
+        return DataTable(
+            id='data-table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records')
+        )
     else:
         return ""
 
