6450ff9eaa38645d168e43be2fc30cca7c1bcfdf
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-12 13:35:13 -0400

    t

--- t.py
+++ t.py
@@ -1,11 +1,13 @@
 from seleniumbase import SB
 
 def scrape_2():
+    print("INSIDE SCRAPE_2")
     url_base = "https://www.nature.com/"
     search_query = "enzymatic degradation plastic"
 
     with SB(uc=True, xvfb=True) as sb:
         sb.driver.uc_open_with_reconnect(url_base, 20)
+        print("OPENED WEBSITE")
         # Get the title of the page
         title = sb.driver.title
         print("Title of the page:", title)
