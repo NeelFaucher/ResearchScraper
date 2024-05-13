c22c543b86f611e493cab7100073cd3eae0c6b25
NeelFaucher <neel.faucher@mail.mcgill.ca>
2024-05-13 17:12:14 -0400

    switched to bs4

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 import dash
 from dash import html, dcc, Input, Output, State
-from dash.exceptions import PreventUpdate
-from tasks import scrape_2
-from bs import scrape_bs
+from bs4 import BeautifulSoup
+import requests
 
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
     html.H1("Joy's Scraper"),
-    html.Div([
-        dcc.Input(id='search-bar', type='text', placeholder='Enter value to search...'),
-        html.Button('Search', id='search-button', n_clicks=0),
-        html.Div(id='search-output')
-    ]),
-    html.Div(id='data-table-container')
+    dcc.Input(id='search-bar', type='text', placeholder='Enter value to search...'),
+    html.Button('Search', id='search-button', n_clicks=0),
+    html.Div(id='search-output')
 ])
 
-def scrape():
-    # Your scraping logic goes here
-    # For demonstration, let's assume scraped_data is a string
-    scraped_data = scrape_2.delay()
-    return scraped_data
+def scrape_bs(search_value):
+    # Send an HTTP request to the URL
+    url = f"https://www.nature.com/search?q={search_value}&order=relevance"
+    response = requests.get(url)
+
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the specific <a> tag by its text content
+    link = soup.find('a', text="Natural diversity screening, assay development, and characterization of nylon-6 enzymatic depolymerization")
+
+    # Extract the link if it exists
+    if link:
+        href = link.get('href')
+        return href
+    else:
+        return "Link not found."
 
 @app.callback(
     Output('search-output', 'children'),
@@ -31,21 +39,10 @@ def scrape():
 def scrape_data(n_clicks, search_value):
     if n_clicks > 0:
         # Call the scrape function and return its result
-        scraped_data = scrape_bs
+        scraped_data = scrape_bs(search_value)
         return html.Div(scraped_data)
     else:
-        raise PreventUpdate
-
-@app.callback(
-    Output('data-table-container', 'children'),
-    [Input('search-output', 'children')]
-)
-def display_data_table(scraped_data):
-    if scraped_data is None:
-        # Data not fetched yet, show loading spinner
-        return html.Div("Loading...", style={'textAlign': 'center'})
-    else:
-        return html.Div(scraped_data, style={'color': 'blue'})
+        return ""
 
 if __name__ == '__main__':
     app.run_server(debug=True)
